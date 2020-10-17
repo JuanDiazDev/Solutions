76ed984a6d3e37a4583d9ac9b5fdc39475096a7a
Juan Diaz <juanmanuel32@ciencias.unam.mx>
2020-10-17 17:09:40 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.io.IOException;
 
-public class Marbles{
+public class Main{
     public static void main(String [] args) throws IOException{
 	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	String line;
